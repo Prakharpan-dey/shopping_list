fbf4d0a8819709c28ad0648281bd7b0905c7991f
Prakharpan-dey <prakharpandey067@gmail.com>
2024-11-20 00:51:54 +0530

    Fetching data

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
-import 'package:shopping_list/models/grocery_item.dart';
 import 'package:http/http.dart' as http;
 
 class NewItem extends StatefulWidget {
@@ -19,27 +18,31 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formkey.currentState!.validate()) {
       _formkey.currentState!.save();
-      final url = Uri.https('shopping-list-a56f1-default-rtdb.firebaseio.com','shopping-list.json');
-      http.post(url,headers: {
-        'Content-Type': 'application/json',
-      }, body: json.encode(
+      final url = Uri.https('shopping-list-a56f1-default-rtdb.firebaseio.com',
+          'shopping-list.json');
+      final response = await http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode(
           {
             'name': _enteredName,
             'quantity': _enteredQuantity,
             'category': _selectedCategory.title,
-          }
+          },
         ),
       );
-      // Navigator.of(context).pop(
-      //   GroceryItem(
-      //       id: DateTime.now().toString(),
-      //       name: _enteredName,
-      //       quantity: _enteredQuantity,
-      //       category: _selectedCategory),
-      // );
+      print(response.body);
+      print(response.statusCode);
+
+      if(!context.mounted){
+        return;
+      }
+      Navigator.of(context).pop();
     }
   }
 
